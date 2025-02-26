d7b51d759ae1e9e17d31509fc4b2a3824e75019b
Aamna Khan <aamnakhan512@gmail.com>
2025-02-27 01:32:13 +0500

    os-shell builtin

--- shell.py
+++ shell.py
@@ -1,4 +1,15 @@
-builtin_cmd=["echo", "type", "exit 0"]
+import os
+builtin_cmd=["echo", "type", "exit 0", "pwd", "cd"]
+extensions=[".exe"]
+def getPath(cmd):
+    paths=os.getenv("PATH").split(os.pathsep)
+    for path in paths:
+        for ext in extensions:
+            full_path=os.path.join(path, cmd+ext)
+            if os.path.exists(full_path):
+                return full_path
+    return None
+            
 while True:
     user_input=input("$ ")
     cmd_list=user_input.split(" ")
@@ -9,9 +20,17 @@ while True:
              print(*arg)
         case ["type", *arg]:
             str=" ".join(arg)
+            full_path=getPath(str)
             if str in builtin_cmd:
                 print(f"{str} : is a shell builtin")
+            elif full_path:
+                print(f"{str} : is {full_path}")
             else:
                 print(f"{str} : is not a builtin shell")
+        case ["pwd"]:
+            print(os.getcwd())
+        case ["cd", *arg]:
+            cmd=" ".join(arg)
+            os.chdir(cmd)
         case _:
             print(f"{user_input}: command not found")
